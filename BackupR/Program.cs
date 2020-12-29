ea79ba34d8c5edc9af1d083a9a123c22f53eb4ef
Julian Tekook <Julian@Tekook.de>
2020-12-29 22:56:13 +0100

    use dev ftp for testing

--- BackupR/Program.cs
+++ BackupR/Program.cs
@@ -1,5 +1,4 @@
-﻿using FluentFTP;
-using System;
+﻿using System;
 using System.Threading.Tasks;
 using Tekook.BackupR.Lib.Contracts;
 using Tekook.BackupR.Lib.Ftp;
@@ -12,12 +11,14 @@ namespace Tekook.BackupR
         {
             FtpConfig config = new FtpConfig()
             {
-                Host = "ftpserver",
-                Path = "/"
+                Host = "ftp.dev.local",
+                Path = "/",
+                Username = "docker-dev",
+                Password = "docker-dev"
             };
             FTPProvider provider = new FTPProvider(config);
             IContainer root = await provider.Read();
-            foreach(IContainer sub in root.Containers)
+            foreach (IContainer sub in root.Containers)
             {
                 Console.WriteLine(sub);
             }
