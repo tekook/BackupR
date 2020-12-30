94e2d62ff6a2aebf11318bf12b02366c7827dc34
Julian Tekook <julian.tekook@toefi.de>
2020-12-30 08:44:49 +0100

    Add readable bytes

--- BackupR/Program.cs
+++ BackupR/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Tekook.BackupR.Lib;
 using Tekook.BackupR.Lib.Contracts;
 using Tekook.BackupR.Lib.Ftp;
 
@@ -23,22 +24,20 @@ namespace Tekook.BackupR
             long max = 1024 * 1024 * 1024;
             foreach (IContainer sub in root.Containers)
             {
-                if (sub.Name == "home")
+                Console.WriteLine($"Checking {sub.Name} with Size: {sub.Size.ToReadableBytes()}");
+                if (sub.Size > max)
                 {
-                    if (sub.Size > max)
+                    var files = sub.Items.OrderBy(x => x.Date).ToList();
+                    var toDelete = new List<IItem>();
+                    while (files.Sum(x => x.Size) > max && files.Count > 0)
                     {
-                        var files = sub.Items.OrderBy(x => x.Date).ToList();
-                        var toDelete = new List<IItem>();
-                        while (files.Sum(x => x.Size) > max && files.Count > 0)
-                        {
-                            toDelete.Add(files[0]);
-                            files.RemoveAt(0);
-                        }
-                        foreach (var item in toDelete)
-                        {
-                            Console.WriteLine($"Deleting {item.FullName}");
-                            await item.Delete();
-                        }
+                        toDelete.Add(files[0]);
+                        files.RemoveAt(0);
+                    }
+                    foreach (var item in toDelete)
+                    {
+                        Console.WriteLine($"Deleting {item.FullName} -> {item.Size.ToReadableBytes()}");
+                        await item.Delete();
                     }
                 }
             }
