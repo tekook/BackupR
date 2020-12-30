28af5d42f67d749d6b34fc93d02dd51183e78301
Julian Tekook <julian.tekook@toefi.de>
2020-12-30 09:15:55 +0100

    use abstract container class

---
+++ BackupR/Lib/Container.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Tekook.BackupR.Lib.Contracts;
+
+namespace Tekook.BackupR.Lib
+{
+    public abstract class Container<TProvider, TContainer, TItem> : IContainer where TItem : IItem where TContainer : IContainer where TProvider : IProvider
+    {
+        /// <inheritdoc/>
+        IEnumerable<IContainer> IContainer.Containers => (IEnumerable<IContainer>)this.Containers;
+
+        /// <summary>
+        /// Child containers of this container.
+        /// </summary>
+        public List<TContainer> Containers { get; set; } = new List<TContainer>();
+
+        /// <inheritdoc/>
+        IEnumerable<IItem> IContainer.Items => (IEnumerable<IItem>)this.Items;
+
+        /// <summary>
+        /// Directy child items this container has.
+        /// </summary>
+        public List<TItem> Items { get; set; } = new List<TItem>();
+
+        /// <inheritdoc/>
+        public string Name { get; }
+
+        /// <inheritdoc/>
+        public string Path { get; }
+
+        /// <summary>
+        /// Provider this container belongs to.
+        /// </summary>
+        public TProvider Provider { get; }
+
+        /// <inheritdoc/>
+        IProvider IContainer.Provider => this.Provider;
+
+        /// <inheritdoc/>
+        public long Size => this.Items.Sum(x => x.Size) + this.Containers.Sum(x => x.Size);
+
+        /// <summary>
+        /// Creates a new Container with the given provider.
+        /// </summary>
+        /// <param name="provider">Provider to use.</param>
+        /// <param name="path">Path this container sits on.</param>
+        public Container(TProvider provider, string path)
+        {
+            this.Provider = provider;
+            this.Path = path ?? throw new ArgumentNullException(nameof(path));
+            this.Name = System.IO.Path.GetFileName(path);
+        }
+    }
+}
--- BackupR/Lib/Ftp/FtpContainer.cs
+++ BackupR/Lib/Ftp/FtpContainer.cs
@@ -1,58 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Tekook.BackupR.Lib.Contracts;
+﻿using Tekook.BackupR.Lib.Contracts;
 
 namespace Tekook.BackupR.Lib.Ftp
 {
     /// <summary>
     /// Implementation of an FtpContainer
     /// </summary>
-    public class FtpContainer : IContainer
+    public class FtpContainer : Container<FtpProvider, FtpContainer, FtpItem>, IContainer
     {
-        /// <summary>
-        /// Child Containers this Container holds.
-        /// </summary>
-        public List<FtpContainer> Containers { get; } = new List<FtpContainer>();
-
-        /// <inheritdoc/>
-        IEnumerable<IContainer> IContainer.Containers => this.Containers;
-
-        /// <summary>
-        /// Items this container holds.
-        /// </summary>
-        public List<FtpItem> Items { get; } = new List<FtpItem>();
-
-        /// <inheritdoc/>
-        IEnumerable<IItem> IContainer.Items => this.Items;
-
-        /// <inheritdoc/>
-        public string Name { get; }
-
-        /// <inheritdoc/>
-        public string Path { get; }
-
-        /// <summary>
-        /// Provider this container belongs to.
-        /// </summary>
-        public FtpProvider Provider { get; }
-
-        /// <inheritdoc/>
-        IProvider IContainer.Provider => this.Provider;
-
-        /// <inheritdoc/>
-        public long Size => this.Items.Sum(x => x.Size) + this.Containers.Sum(x => x.Size);
-
         /// <summary>
         /// Creates a new <see cref="FtpContainer"/>
         /// </summary>
         /// <param name="provider">Provider this container belongs to.</param>
         /// <param name="path">Path this container is set on.</param>
-        public FtpContainer(FtpProvider provider, string path)
+        public FtpContainer(FtpProvider provider, string path) : base(provider, path)
         {
-            this.Provider = provider ?? throw new ArgumentNullException(nameof(provider));
-            this.Path = path ?? throw new ArgumentNullException(nameof(path));
-            this.Name = System.IO.Path.GetFileName(path);
         }
     }
 }
