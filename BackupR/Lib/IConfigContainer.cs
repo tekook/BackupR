c7d66c73351b6d60e2e4e0286146385d390f6a8a
Julian Tekook <Julian@Tekook.de>
2021-01-17 23:00:44 +0100

    implement handling logics

--- BackupR/Lib/IConfigContainer.cs
+++ BackupR/Lib/IConfigContainer.cs
@@ -1,9 +1,16 @@
-﻿namespace Tekook.BackupR.Lib
+﻿using Config.Net;
+
+namespace Tekook.BackupR.Lib
 {
     public interface IConfigContainer
     {
-        long MaxFiles { get; }
+        long? MaxFiles { get; }
+
         string MaxSize { get; }
+
+        [Option(DefaultValue = 0L)]
+        long MinFiles { get; }
+
         string Path { get; }
     }
 }
--- BackupR/Verbs/Cleanup/CleanupVerb.cs
+++ BackupR/Verbs/Cleanup/CleanupVerb.cs
@@ -1,4 +1,5 @@
-﻿using Config.Net;
+﻿using ByteSizeLib;
+using Config.Net;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,27 +23,64 @@ namespace Tekook.BackupR.Verbs.Cleanup
             IProvider provider = Lib.Resolver.ResolveProvider(this.Config, this.Options);
             IContainer root = await provider.GetRoot();
 
-            long max = 1024 * 1024 * 1024;
-            foreach (IContainer sub in root.Containers)
+            foreach (IConfigContainer configContainer in this.Config.Containers)
             {
-                Console.WriteLine($"Checking {sub.Name} with Size: {ByteSizeLib.ByteSize.FromBytes(sub.Size)}");
-                if (sub.Size > max)
+                IContainer container = root.AllContainers.Where(x => x.Path == root.Path + configContainer.Path).FirstOrDefault();
+                if (container == null)
                 {
-                    var files = sub.Items.OrderBy(x => x.Date).ToList();
-                    var toDelete = new List<IItem>();
-                    while (files.Sum(x => x.Size) > max && files.Count > 0)
-                    {
-                        toDelete.Add(files[0]);
-                        files.RemoveAt(0);
-                    }
-                    foreach (var item in toDelete)
-                    {
-                        Console.WriteLine($"Deleting {item.Path} -> {ByteSizeLib.ByteSize.FromBytes(item.Size)}");
-                        await item.Delete();
-                    }
+                    Console.WriteLine($"Container not found for {configContainer.Path}");
+                    continue;
+                }
+                if (configContainer.MaxSize != null)
+                {
+                    await HandleMaxSize(configContainer, container);
+                }
+                if (configContainer.MaxFiles != null)
+                {
+                    await HandleMaxFiles(configContainer, container);
                 }
             }
             return 0;
+        }
+
+        private async Task HandleMaxFiles(IConfigContainer configContainer, IContainer container)
+        {
+            Console.WriteLine($"Handling {configContainer.Path} with MaxFiles: {configContainer.MaxFiles}. Current: {container.Items.Count()}");
+            var files = container.Items.OrderBy(x => x.Date).ToList();
+            var toDelete = new List<IItem>();
+            while (files.Count > configContainer.MinFiles && files.Count > configContainer.MaxFiles)
+            {
+                toDelete.Add(files[0]);
+                files.RemoveAt(0);
+            }
+            foreach (var item in toDelete)
+            {
+                Console.WriteLine($"Deleting {item.Path} -> {ByteSize.FromBytes(item.Size)}");
+                //await item.Delete();
+            }
+        }
+
+        private async Task HandleMaxSize(IConfigContainer configContainer, IContainer container)
+        {
+            ByteSize max = ByteSize.Parse(configContainer.MaxSize);
+            ByteSize containerSize = ByteSize.FromBytes(container.Size);
+            Console.WriteLine($"Handling {configContainer.Path} with MaxSize: {max}. Current: {containerSize}");
+            if (containerSize > max)
+
+            {
+                var files = container.Items.OrderBy(x => x.Date).ToList();
+                var toDelete = new List<IItem>();
+                while (files.Sum(x => x.Size) > max.Bytes && files.Count > configContainer.MinFiles)
+                {
+                    toDelete.Add(files[0]);
+                    files.RemoveAt(0);
+                }
+                foreach (var item in toDelete)
+                {
+                    Console.WriteLine($"Deleting {item.Path} -> {ByteSize.FromBytes(item.Size)}");
+                    //await item.Delete();
+                }
+            }
         }
     }
 }
