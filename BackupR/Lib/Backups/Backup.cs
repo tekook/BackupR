36288c8f155c6573c7a1b8a3084ff98c8ae39140
Julian Tekook <Julian@Tekook.de>
2021-01-23 20:11:27 +0100

    ... I hate the gitignore

---
+++ BackupR/Lib/Backups/Backup.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Tekook.BackupR.Lib.Backups
+{
+    internal abstract class Backup
+    {
+        /// <summary>
+        /// Creates the backup.
+        /// </summary>
+        /// <returns>Path of the created backup file.</returns>
+        public abstract Task<FileInfo> CreateBackup();
+    }
+}
---
+++ BackupR/Lib/Backups/CommandBackup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using Tekook.BackupR.Lib.Config;
+
+namespace Tekook.BackupR.Lib.Backups
+{
+    internal class CommandBackup : Backup
+    {
+        protected ICommandBackup Settings { get; set; }
+
+        public CommandBackup(ICommandBackup settings)
+        {
+            this.Settings = settings ?? throw new ArgumentNullException(nameof(settings));
+        }
+
+        public override async Task<FileInfo> CreateBackup()
+        {
+            var process = new Process
+            {
+                StartInfo =
+                {
+                    FileName = this.Settings.Command,
+                    Arguments = this.Settings.Arguments,
+                    WorkingDirectory = this.Settings.WorkingDirectory,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                    RedirectStandardOutput = true
+                }
+            };
+            process.Start();
+            await process.WaitForExitAsync();
+            if (process.ExitCode == 0)
+            {
+                string o = await process.StandardOutput.ReadLineAsync();
+                return new FileInfo(o);
+            }
+            return null;
+        }
+    }
+}
