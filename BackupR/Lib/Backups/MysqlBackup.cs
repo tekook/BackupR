d992b935d32d22a4c7b653688b07628c266cede0
jute <julian@tekook.de>
2021-04-27 22:54:20 +0200

    Start implementing mysql backup

---
+++ BackupR/Lib/Backups/MysqlBackup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tekook.BackupR.Lib.Config;
+
+namespace Tekook.BackupR.Lib.Backups
+{
+    internal class MysqlBackup : Backup
+    {
+        protected IMysqlBackup Settings { get; set; }
+        public MysqlBackup(IMysqlBackup settings)
+        {
+            this.Settings = settings ?? throw new ArgumentNullException(nameof(settings));
+        }
+        public override Task<FileInfo> CreateBackup()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- BackupR/Lib/Config/IMysqlBackup.cs
+++ BackupR/Lib/Config/IMysqlBackup.cs
@@ -3,5 +3,11 @@
     public interface IMysqlBackup : IBackup
     {
         public string MysqlDumpPath { get; }
+        public bool AddLocks { get; }
+        public bool FlushPrivilegies { get; }
+        public bool Routines { get; }
+        public bool Events { get; }
+        public bool Triggers { get; }
+        public string DbName { get; }
     }
 }
