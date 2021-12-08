4edb20d5a89e493bc64a4e21b6c6c54b513ff97c
jute <julian@tekook.de>
2021-12-08 15:42:19 +0100

    add exceptions for the config property

--- BackupR/Verbs/CleanupOptions.cs
+++ BackupR/Verbs/CleanupOptions.cs
@@ -1,14 +1,32 @@
 ﻿using CommandLine;
+using System;
+using System.IO;
 using Tekook.BackupR.Lib.Contracts;
 using Tekook.VerbR.Contracts;
 
 namespace Tekook.BackupR.Verbs
 {
-    [Verb("cleanup", true)]
+    [Verb("cleanup", HelpText = "Starts the cleanup process.")]
     internal class CleanupOptions : ICanValidateOnly, IOptions
     {
+        private string config;
+
         [Option('c', "config", Required = true)]
-        public string Config { get; set; }
+        public string Config
+        {
+            get => config; set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException(nameof(Config));
+                }
+                if (!File.Exists(value))
+                {
+                    throw new ArgumentException("Config file does not exist!", nameof(Config));
+                }
+                config = value;
+            }
+        }
 
         [Option("validation-only", Default = false)]
         public bool ValidationOnly { get; set; }
