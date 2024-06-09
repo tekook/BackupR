1bffe6102c1c49af287d58bb7cd51a5170e8ceab
jute <julian@tekook.de>
2024-06-09 16:56:19 +0200

    fix some warnings

--- BackupR/Lib/Providers/FsProvider.cs
+++ BackupR/Lib/Providers/FsProvider.cs
@@ -45,7 +45,9 @@ namespace Tekook.BackupR.Lib.Providers
         }
 
         /// <inheritdoc/>
+#pragma warning disable CS1998 // Bei der asynchronen Methode fehlen "await"-Operatoren. Die Methode wird synchron ausgeführt.
         public async Task Delete(IItem item)
+#pragma warning restore CS1998 // Bei der asynchronen Methode fehlen "await"-Operatoren. Die Methode wird synchron ausgeführt.
         {
             if (item.Container.Provider != this)
             {
@@ -120,7 +122,9 @@ namespace Tekook.BackupR.Lib.Providers
         }
 
         /// <inheritdoc/>
+#pragma warning disable CS1998 // Bei der asynchronen Methode fehlen "await"-Operatoren. Die Methode wird synchron ausgeführt.
         public async Task Upload(FileInfo file, IContainer target, string name = null)
+#pragma warning restore CS1998 // Bei der asynchronen Methode fehlen "await"-Operatoren. Die Methode wird synchron ausgeführt.
         {
             try
             {
@@ -141,7 +145,9 @@ namespace Tekook.BackupR.Lib.Providers
         }
 
         /// <inheritdoc/>
+#pragma warning disable CS1998 // Bei der asynchronen Methode fehlen "await"-Operatoren. Die Methode wird synchron ausgeführt.
         public async Task Validate()
+#pragma warning restore CS1998 // Bei der asynchronen Methode fehlen "await"-Operatoren. Die Methode wird synchron ausgeführt.
         {
             if (!Directory.Exists(this.RootPath))
             {
--- BackupR/Program.cs
+++ BackupR/Program.cs
@@ -21,11 +21,13 @@ namespace Tekook.BackupR
             }
         }
 
+#pragma warning disable CS1998 // Bei der asynchronen Methode fehlen "await"-Operatoren. Die Methode wird synchron ausgeführt.
         private static async Task Main(string[] args)
+#pragma warning restore CS1998 // Bei der asynchronen Methode fehlen "await"-Operatoren. Die Methode wird synchron ausgeführt.
         {
             Parser.Default.ParseArguments<CleanupOptions, BackupOptions>(args)
-                .WithParsed<CleanupOptions>(o => (new CleanupVerb(o)).Invoke())
-                .WithParsed<BackupOptions>(o => (new BackupVerb(o)).Invoke())
+                .WithParsed<CleanupOptions>(o => new CleanupVerb(o).Invoke())
+                .WithParsed<BackupOptions>(o => new BackupVerb(o).Invoke())
                 .WithNotParsed(HandleParseError);
         }
     }
